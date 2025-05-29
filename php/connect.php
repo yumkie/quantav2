11d95a6c83ab93cf02f238c20f27aedec2f5b221
yumkie <masanaguy@gmail.com>
2025-05-30 01:47:53 +0700

    Delete php directory

--- php/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'mysql-8.0';
-$db = 'Users';
-$username  = 'root';
-$password = '';
-$link = mysqli_connect($host, $username, $password, $db);
-?>
--- php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start(); //запуск сессии
-    session_destroy(); //уничтоэение сессии
-    header("Location: ../html/log_in.php"); //редирект на страницу авторизации
-    die();
-?>
