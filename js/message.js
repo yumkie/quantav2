37f3dd435fc9b228f5b965ff1188764062582ae0
yumkie <masanaguy@gmail.com>
2025-05-30 01:46:35 +0700

    Delete js directory

--- js/message.js
+++
@@ -1,5 +0,0 @@
-function ds(Chat) {
-        $('.chat_container_view').css('display', 'none');
-        $(`#chat-${Chat}`).css('display', 'block');
-        $('.chat_container').css('display', 'none');
-}
--- js/pass_view.js
+++
@@ -1,9 +0,0 @@
-function view() {
-    if ($('#password-input').attr('type') == 'password'){
-		$('.view_password').attr('class', 'no_view_password')
-		$('#password-input').attr('type', 'text');
-	} else {
-        $('.no_view_password').attr('class', 'view_password')
-		$('#password-input').attr('type', 'password');
-	}
-};
